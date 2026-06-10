170600b256fcfc1c7d8d5b09a74b52133fb3cebc
mayank gohiya <dtmayankgohiya@gmail.com>
2026-06-11 00:34:52 +0530

    fix: update Blog Website link to correct URL for proper navigation

--- src/Component/ProjectComponent.jsx
+++ src/Component/ProjectComponent.jsx
@@ -26,7 +26,7 @@ const ProjectComponent = () => {
       img: img,
       title: "Blog Website",
       desc: "Blog Reading Website",
-      link: "https://devblogwebsite.netlify.app",
+      link: "https://blogsreadweb.netlify.app",
     },
   ]);
 
